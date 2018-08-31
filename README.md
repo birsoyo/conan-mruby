a28b984c3fc8bfec9f0fb1b9809b9a8297aed9cc
Orhun Birsoy <orhunbirsoy@gmail.com>
2018-08-30 23:20:45 -0600

    Minor improvements

---
+++ README.md
@@ -0,0 +1,8 @@
+## Package Status
+
+[ ![Download](https://api.bintray.com/packages/orhun/sesame/mruby%3Asesame/images/download.svg) ](https://bintray.com/orhun/sesame/mruby%3Asesame/_latestVersion)
+
+| Windows |
+|:--------:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/a6re17mrrunls17q?svg=true)](https://ci.appveyor.com/project/birsoyo/conan-mruby)
+|
--- build-sesame.py
+++ build-sesame.py
@@ -9,21 +9,7 @@ def main():
     builder = build_template_default.get_builder()
 
     build_for = os.getenv('SESAME_BUILD_FOR', '')
-
-    if build_for == 'windows':
-        filtered_builds = []
-        build_types = split_colon_env('CONAN_BUILD_TYPES')
-        if 'RelWithDebInfo' in build_types and not 'Release' in build_types:
-            for settings, options, env_vars, build_requires, reference in builder.items:
-                if settings['build_type'] == 'Debug':
-                    filtered_builds.append([settings, options, env_vars, build_requires])
-                    s = settings.copy()
-                    s['build_type'] = 'RelWithDebInfo'
-                    s['compiler.runtime'] = 'MD'
-                    filtered_builds.append([s, options, env_vars, build_requires])
-            builder.builds = filtered_builds
-
-    elif build_for == 'emscripten':
+    if build_for == 'emscripten':
         builder.builds = []
         default_build_types = ["Release", "Debug"]
         build_types = split_colon_env('CONAN_BUILD_TYPES') or default_build_types
